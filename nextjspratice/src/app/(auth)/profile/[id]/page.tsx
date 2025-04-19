dc394a1d1f7df767b4f18ad5f340205d361c77d3
Ashok Gunji <ashokgunji0202@gmail.com>
2025-04-19 17:29:33 +0530

    add me api for userDetails

--- nextjspratice/src/app/(auth)/profile/[id]/page.tsx
+++ nextjspratice/src/app/(auth)/profile/[id]/page.tsx
@@ -4,7 +4,7 @@ export default function Profile({ params }:
         <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
             <h1>Profile</h1>
             <hr />
-            <p className="text-4xl">profile page <span className="p-2 ml-2 rounded bg-orange-400 text-black">{params.id}</span>a</p>
+            <p className="text-4xl">profile page <span className="p-2 ml-2 rounded bg-orange-400 text-black">{params.id}</span></p>
         </div>
     )
 }
---
+++ nextjspratice/src/middleware.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+
+    // Using `request.nextUrl.pathname` to get the path of the request
+    // Instead of using `request.url` because it includes the query string
+    // and we don't want to match the query string in this case
+    const path = request.nextUrl.pathname;
+
+    // Check if the user is logged in or not
+    const isPublicPath = path === '/login' || path === '/signup';
+
+    const token = request.cookies.get('token')?.value || '';
+
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl));
+    }
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl));
+    }
+
+  
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    "/",
+    "/profile/:path*",
+    "/login",
+    "/signup"
+  ]
+}
